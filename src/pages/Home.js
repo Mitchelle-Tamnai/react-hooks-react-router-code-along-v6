65b975fd64f9fb7dd56f40fa4a377ffa265b8328
MitchelleTamnai <cheptoektamnai@gmail.com>
2024-05-11 10:23:12 +0300

    Commit changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,13 @@
 import { useState, useEffect } from "react";
 import UserCard from "../components/UserCard";
+import NavBar from "../components/NavBar";
 
 function Home() {
   const [users, setUsers] = useState([])
 
   useEffect(() =>{
-    fetch("http://localhost:4000/users")
-      .then(r => r.json())
+    fetch("http://localhost:4001/users")
+      .then(resp => resp.json())
       .then(data => setUsers(data))
       .catch(error => console.error(error));
   }, []);
@@ -18,7 +19,7 @@ function Home() {
   return (
     <>
       <header>
-        {/* place NavBar here */}
+        <NavBar />
       </header>
       <main>
         <h1>Home!</h1>
